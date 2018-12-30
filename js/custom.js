06f8ab2ff67f08571a256a1e4c85baea8e4a8806
sagar <sagarkumarjha96@gmail.com>
2018-12-30 20:54:49 +0530

    first commit

---
+++ js/custom.js
@@ -0,0 +1,44 @@
+var add=document.getElementById('add');
+var removeall = document.getElementById('removeall');
+var list = document.getElementById('list');
+
+removeall.onclick = function(){
+    list.innerHTML='';
+}
+
+add.onclick = function(){
+    addlis(list);
+    document.getElementById('userinput').value='';
+    document.getElementById('userinput').focus();
+}
+
+function addlis(targetUl){
+    var inputText = document.getElementById('userinput').value;
+    var li = document.createElement('li');
+    var textNode = document.createTextNode(inputText + ' ');
+    var removeButton = document.createElement('button');
+    if(inputText !== ''){
+        removeButton.className = 'btn btn-xs btn-danger';
+        removeButton.innerHTML = ' &times; ';
+        removeButton.setAttribute('onclick','removeMe(this)');
+        li.appendChild(textNode);
+        li.appendChild(removeButton);
+        targetUl.appendChild(li);
+    }else{
+        alert("please enter a todo");
+    }
+}
+function removeMe(item){
+    var p = item.parentElement;
+    p.parentElement.removeChild(p);
+}
+
+
+
+
+
+
+
+
+
+
